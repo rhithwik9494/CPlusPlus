d5e3fd4b64633bfb2c6b15825746bad6cddc0cd9
rhithwik9494 <coder9494@gmail.com>
2025-03-03 00:12:27 +0530

    Add files via upload
    This C++ program demonstrate that how the input from user side (using cin function) in array.

---
+++ Basic/2 Take_data_from_user_side.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int arr[5];
+ cout<<" Eneter element in array "<<endl;
+ for(int i=0;i<5;i++)
+ {
+   cin>>arr[i];
+ }
+ cout<<" Array element are "<<endl;
+ for(int i=0;i<5;i++)
+ {
+  cout<<arr[i]<<" ";
+ }
+}
