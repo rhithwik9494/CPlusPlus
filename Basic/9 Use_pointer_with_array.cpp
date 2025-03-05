66f1bb13f8e03c8fa63ab148ddce09b2e6298ef8
rhithwik9494 <coder9494@gmail.com>
2025-03-05 23:36:30 +0530

    Add files via upload
    This program is about pointer how  to use pointer in c++.

---
+++ Basic/9 Use_pointer_with_array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a[5]={23,45,56,67,78};
+ cout<<a<<endl;
+ cout<<*a<<endl;
+ cout<<*(a+2)<<endl;
+ cout<<a+2<<endl;
+ cout<<(*a)+3<<endl;
+}
