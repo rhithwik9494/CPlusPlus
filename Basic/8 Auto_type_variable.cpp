3dadccd9cdd85a6a9508d632c3adee6d9ba4e340
rhithwik9494 <coder9494@gmail.com>
2025-03-05 23:29:54 +0530

    Add files via upload
    This program about auto type variable auto is a key word  which allows the compiler to automatically detect the type of a variable based on its initializer.

---
+++ Basic/8 Auto_type_variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ auto p=45.5;
+ auto a=22;
+ auto c='t';
+ cout<<"it's integer value a = "<<a<<endl;
+ cout<<"it's float value p = "<<p<<endl;
+ cout<<"it's character c = "<<c<<endl;
+}
