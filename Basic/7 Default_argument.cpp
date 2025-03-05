e767a8f35c010c8f36b4763dd2703a1a81ea7757
rhithwik9494 <coder9494@gmail.com>
2025-03-05 23:24:59 +0530

    Add files via upload
    This program demonstrate that create how to take default argument in parameter

---
+++ Basic/7 Default_argument.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void display(int a=4,int b=9,int c=90)
+{
+ cout<<"value of a is "<<a<< " and b is "<<b<<" and c is "<<c;
+}
+int main()
+{
+display(23,45);
+}
