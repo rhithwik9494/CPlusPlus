b9d6b49ddf9b1cde2622010d4bf593aa939d1fb7
rhithwik9494 <coder9494@gmail.com>
2025-03-08 00:34:55 +0530

    Add files via upload
    This program demonstrate that the how to concatenate two strings.

---
+++ Basic/25 Concatenate_two_string.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string fname="john";
+    string lname="william";
+    cout<<" Name :- "<<fname + lname;   /// (+) concatenate oprater
+}
