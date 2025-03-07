ab6755c0aa587269cc32f5c8e67fc42252fc1826
rhithwik9494 <coder9494@gmail.com>
2025-03-08 00:38:49 +0530

    Add files via upload
    This program demonstrate that how to compare to strings.

---
+++ Basic/26 Compare_two_string.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s1,s2;
+    cout<<" Enter first string :- "<<endl;
+    cin>>s1;
+    cout<<" Enter second string :- "<<endl;
+    cin>>s2;
+    if(s1==s2)
+    {
+        cout<<" Both string are same"<<endl;
+    }
+    else
+    {
+        cout<<" Both string are not equal "<<endl;
+    }
+}
